20df80cbc16563d932f68829500da93a4eff1c26
alexoshri <alexoshri@gmail.com>
2016-09-26 21:57:05 +0300

    test commit

---
+++ build/tum_ardrone/CMakeFiles/tum_ardrone_gencfg.dir/cmake_clean.cmake
@@ -0,0 +1,23 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/tum_ardrone_gencfg"
+  "/home/alexpoz/catkin_ws/devel/include/tum_ardrone/AutopilotParamsConfig.h"
+  "/home/alexpoz/catkin_ws/devel/share/tum_ardrone/docs/AutopilotParamsConfig.dox"
+  "/home/alexpoz/catkin_ws/devel/share/tum_ardrone/docs/AutopilotParamsConfig-usage.dox"
+  "/home/alexpoz/catkin_ws/devel/lib/python2.7/dist-packages/tum_ardrone/cfg/AutopilotParamsConfig.py"
+  "/home/alexpoz/catkin_ws/devel/share/tum_ardrone/docs/AutopilotParamsConfig.wikidoc"
+  "/home/alexpoz/catkin_ws/devel/include/tum_ardrone/GUIParamsConfig.h"
+  "/home/alexpoz/catkin_ws/devel/share/tum_ardrone/docs/GUIParamsConfig.dox"
+  "/home/alexpoz/catkin_ws/devel/share/tum_ardrone/docs/GUIParamsConfig-usage.dox"
+  "/home/alexpoz/catkin_ws/devel/lib/python2.7/dist-packages/tum_ardrone/cfg/GUIParamsConfig.py"
+  "/home/alexpoz/catkin_ws/devel/share/tum_ardrone/docs/GUIParamsConfig.wikidoc"
+  "/home/alexpoz/catkin_ws/devel/include/tum_ardrone/StateestimationParamsConfig.h"
+  "/home/alexpoz/catkin_ws/devel/share/tum_ardrone/docs/StateestimationParamsConfig.dox"
+  "/home/alexpoz/catkin_ws/devel/share/tum_ardrone/docs/StateestimationParamsConfig-usage.dox"
+  "/home/alexpoz/catkin_ws/devel/lib/python2.7/dist-packages/tum_ardrone/cfg/StateestimationParamsConfig.py"
+  "/home/alexpoz/catkin_ws/devel/share/tum_ardrone/docs/StateestimationParamsConfig.wikidoc"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang)
+  INCLUDE(CMakeFiles/tum_ardrone_gencfg.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
---
+++ build/tum_ardrone/CMakeFiles/tum_ardrone_generate_messages_lisp.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/tum_ardrone_generate_messages_lisp"
+  "/home/alexpoz/catkin_ws/devel/share/common-lisp/ros/tum_ardrone/msg/filter_state.lisp"
+  "/home/alexpoz/catkin_ws/devel/share/common-lisp/ros/tum_ardrone/srv/SetMaxControl.lisp"
+  "/home/alexpoz/catkin_ws/devel/share/common-lisp/ros/tum_ardrone/srv/SetInitialReachDistance.lisp"
+  "/home/alexpoz/catkin_ws/devel/share/common-lisp/ros/tum_ardrone/srv/SetStayTime.lisp"
+  "/home/alexpoz/catkin_ws/devel/share/common-lisp/ros/tum_ardrone/srv/SetReference.lisp"
+  "/home/alexpoz/catkin_ws/devel/share/common-lisp/ros/tum_ardrone/srv/SetStayWithinDistance.lisp"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang)
+  INCLUDE(CMakeFiles/tum_ardrone_generate_messages_lisp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
